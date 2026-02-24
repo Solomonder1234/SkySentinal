7a7bd9c81df0073f418b38cd9d26fb055d3f3c8a
Leo Bernstein <leobernstein@Leos-MacBook-Pro.local>
2026-02-24 17:32:54 -0500

    feat(deps): upgrade all dependencies to newest node 22 versions

--- src/lib/services/MusicService.ts
+++ src/lib/services/MusicService.ts
@@ -28,20 +28,6 @@ export class MusicService {
                 new YtDlpPlugin(),
                 new YouTubePlugin(),
             ],
-            ffmpeg: {
-                path: ffmpegPath,
-                args: {
-                    global: {
-                        loglevel: 'debug'
-                    },
-                    input: {
-                        'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-                        'reconnect': '1',
-                        'reconnect_streamed': '1',
-                        'reconnect_delay_max': '5'
-                    }
-                }
-            }
         });
 
         this.setupEvents();
